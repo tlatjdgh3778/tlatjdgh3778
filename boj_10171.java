129b5ef246494e8000e573c0f9c29213d74c2ec8
tjdgh <71371075+tlatjdgh3778@users.noreply.github.com>
2020-12-30 17:28:59 +0900

    10171(고양이)

---
+++ boj_10171.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class boj_10171
+{
+	public static void main (String[] args) throws IOException
+	{
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+	}
+}
